c2628fa81050a18613598cc764c44736f5796f63
Lee Yik Jiun <yikjiun.lee@switcheo.network>
2020-06-30 13:38:43 +0800

    Add comments to map iteration random test

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -9,20 +9,24 @@ import (
 func TestMapIterationRandom(t *testing.T) {
     n := 10
 
+    // create a map with values from 1 to 10
     m := make(map[int]bool)
     for i := 0; i < n; i++ {
         m[i] = true
     }
 
+    // iterate over map and assign values to xs
     xs := make([]int, 0, n)
     for i := range m {
         xs = append(xs, i)
     }
 
+    // iterate over map again and assign values to ys
     ys := make([]int, 0, n)
     for i := range m {
         ys = append(ys, i)
     }
 
+    // xs is not equal to ys with high probability
     assert.NotEqual(t, xs, ys)
 }
